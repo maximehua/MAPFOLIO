9ada31005f51674f9b6298811c3fad8d208e587a
Nelson <nelsondavis@gmail.com>
2017-12-05 12:50:45 -0500

    pushing final changes

--- src/Components/ProjectsOverview.js
+++ src/Components/ProjectsOverview.js
@@ -15,7 +15,6 @@ const Wrapper = styled.div`
     align-items: center;
 `;
 
-
 class ProjectsOverview extends Component {
     constructor(props) {
         super(props);
@@ -55,6 +54,13 @@ class ProjectsOverview extends Component {
             }
             )
     }
+
+    // const str = "Crème Brulée"
+    // str.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+    // > 'Creme Brulee'
+    // Two things are happening here:
+    // 1. normalize()ing to NFD Unicode normal form decomposes combined graphemes into the combination of simple ones. The è of Crème ends up expressed as e + ̀.
+    // 2. Using a regex character class to match the U+0300 → U+036F range, it is now trivial to globally get rid of the diacritics, which the Unicode standard conveniently groups as the Combining Diacritical Marks Unicode block.
 
     _searchFunction = async () => {
         const raw = await getCurrentProjects(this.props.user.id) //this.state.objects
